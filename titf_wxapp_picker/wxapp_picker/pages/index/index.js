778b0283a1e92a5e510c6a63eb22e5600f91fb9c
wangqingshen <13438347004@163.com>
2017-07-13 17:00:44 +0800

    add yanshi

---
+++ titf_wxapp_picker/wxapp_picker/pages/index/index.js
@@ -0,0 +1,24 @@
+Page({
+  data:{
+    // text:"这是一个页面"
+    picker1Value:0,
+    picker1Range:['北京','上海','广州','深圳'],
+    timeValue:'08:08',
+    dateValue:'2016-10-13'
+  },
+  normalPickerBindchange:function(e){
+    this.setData({
+      picker1Value:e.detail.value
+    })
+  },
+  timePickerBindchange:function(e){
+    this.setData({
+      timeValue:e.detail.value
+    })
+  },
+  datePickerBindchange:function(e){
+    this.setData({
+      dateValue:e.detail.value
+    })
+  }
+})
